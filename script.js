fec99d862f5616f4e89e7b2bae90d21517321f48
RendiAlviana <rendialviyana@gmail.com>
2025-12-29 18:05:40 +0700

    Update mengganti select menjadi accordion

--- script.js
+++ script.js
@@ -15,27 +15,50 @@ Pesan :
   Hubungi saya di DM Instagram, jika ada bug atau saran tentang program ini !!
 */
 
-fetch("soal/sistem.json")
+// Mengambil data file soal untuk di tampilkan di layar
+fetch("sistem.json")
   .then((result) => result.json())
   .then((result) => {
-    let opsiSoal = "";
-    result.forEach((hasil) => {
-      opsiSoal += `<option value="${hasil.nama_file}">
-                    ${hasil.nama_yang_tampil}
-                  </option>`;
+    console.log(result);
+    let isiAccordion = "";
+    result.forEach(({ nama_header, body }) => {
+      isiAccordion += `
+         <div class="accordion-item">
+            <div class="accordion-header">
+              ${nama_header}
+              <img src="svg/angle-small-down.svg" alt="icon-accordion" />
+            </div>
+            <div class="accordion-body">
+              <ul type="none">
+                ${membuatDataAccordion(nama_header, body)}
+              </ul>
+            </div>
+         </div>`;
     });
-    document.querySelector("select[name='pilih-soal']").innerHTML = opsiSoal;
+    document.querySelector(".accordion").innerHTML = isiAccordion;
   });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+function membuatDataAccordion(nama_header, body) {
+  let dataAccordion = "";
+  body.forEach((data) => {
+    dataAccordion += ` <li class="data-accordion" data-file="${nama_header}/${data.nama_file}">${data.nama_yang_tampil}</li>`;
+  });
+  return dataAccordion;
+}
+
+// Argoritma untuk Accordion
+document.addEventListener("click", function ({ target }) {
+  if (target.classList.contains("accordion-header")) {
+    const accordionAktif = document.querySelector(".accordion-aktif");
+    if (accordionAktif && !target.parentElement.classList.contains("accordion-aktif")) accordionAktif.classList.remove("accordion-aktif");
+    target.parentElement.classList.toggle("accordion-aktif");
+  } else if (target.classList.contains("data-accordion")) {
+    const dataAccordionTerpilih = document.querySelector(".data-accordion-terpilih");
+    if (dataAccordionTerpilih) dataAccordionTerpilih.classList.remove("data-accordion-terpilih");
+    target.classList.add("data-accordion-terpilih");
+    document.querySelector(".tombol-dipengaturan .tombol-simpan").style.display = "block";
+  }
+});
 
 let soalKe = 0;
 let jawabanUser = [];
@@ -53,8 +76,7 @@ const seluruhModal = document.querySelectorAll(".modal");
 seluruhTombolModal.forEach((tombolModal) => {
   tombolModal.addEventListener("click", () => {
     seluruhModal.forEach((modal) => {
-      if (tombolModal.dataset.namaTombol == modal.dataset.namaModal)
-        modal.style.display = "flex";
+      if (tombolModal.dataset.namaTombol == modal.dataset.namaModal) modal.style.display = "flex";
     });
   });
 });
@@ -62,61 +84,59 @@ seluruhTombolModal.forEach((tombolModal) => {
 const tutupModal = document.querySelectorAll(".tutup-modal");
 tutupModal.forEach((tutup) => {
   tutup.addEventListener("click", () => {
-    tutup.parentElement.style.display = "none";
+    // modal akan menghilang jika tombol batal sudah ada,
+    // dimana tombol batal akan muncul setelah user tekan tombol simpan
+    if (document.querySelector(".tombol-dipengaturan .tombol-batal").style.display == "block") modalDitutup();
   });
 });
 // ===========================================================
 
 // ------- Modal Pengaturan -------
 // ketika tombol batal di klik
-document.querySelector(".tombol-batal").addEventListener("click", function () {
-  this.parentElement.parentElement.parentElement.style.display = "none";
-});
+document.querySelector(".tombol-batal").addEventListener("click", () => modalDitutup());
 // Ketika tombol simpan diklik
-document
-  .querySelector(".tombol-simpan")
-  .addEventListener("click", async function () {
-    soalKe = 0;
-    this.parentElement.parentElement.parentElement.style.display = "none";
-    dataHasil = await ambilData();
-    console.log(dataHasil);
+document.querySelector(".tombol-simpan").addEventListener("click", async function () {
+  document.querySelector(".tombol-dipengaturan .tombol-batal").style.display = "block";
+  soalKe = 0;
+  modalDitutup();
+  dataHasil = await ambilData();
+  console.log(dataHasil);
 
-    // Mengganti judul soal sesuai data
-    document.querySelector(".judul h3").innerHTML = dataHasil.judul;
-    // Menggati seluruh nomor soal sesuai data yang ada di data base
-    let nomor = "";
-    for (let i = 0; i < dataHasil.jumlah_soal; i++)
-      nomor += `<span>${i + 1}</span>`;
+  // Mengganti judul soal sesuai data
+  document.querySelector(".judul h3").innerHTML = dataHasil.judul;
+  // Menggati seluruh nomor soal sesuai data yang ada di data base
+  let nomor = "";
+  for (let i = 0; i < dataHasil.jumlah_soal; i++) nomor += `<span>${i + 1}</span>`;
 
-    seluruhNoSoal.innerHTML = nomor;
-    seluruhNoSoal.childNodes[soalKe].classList.add("aktif");
-    // Mencetak Lembar Soal Ke documen
-    mencetakLembarSoal(dataHasil);
-    gantiNoAktif();
+  seluruhNoSoal.innerHTML = nomor;
+  seluruhNoSoal.childNodes[soalKe].classList.add("aktif");
+  // Mencetak Lembar Soal Ke documen
+  mencetakLembarSoal(dataHasil);
+  gantiNoAktif();
 
-    // Ketika no soal di klik
-    seluruhNoSoal.childNodes.forEach((noSoal) => {
-      noSoal.addEventListener("click", () => {
-        soalKe = parseInt(noSoal.innerHTML) - 1;
-        console.log(soalKe);
-        mencetakLembarSoal(dataHasil);
-        gantiNoAktif();
-        aktifKanOpsi();
-      });
+  // Ketika no soal di klik
+  seluruhNoSoal.childNodes.forEach((noSoal) => {
+    noSoal.addEventListener("click", () => {
+      soalKe = parseInt(noSoal.innerHTML) - 1;
+      console.log(soalKe);
+      mencetakLembarSoal(dataHasil);
+      gantiNoAktif();
+      aktifKanOpsi();
     });
-
-    // Riset Lembar Soal
-    soalKe = 0;
-    jawabanUser = [];
-    seluruhOpsi.forEach((opsi) => {
-      opsi.checked = false;
-      opsi.disabled = false;
-    });
-    selesai = false;
-    document.querySelector(".tombol-selesai").style.display = "block";
-    document.querySelector(".hasil").style.display = "none";
-    document.querySelector(".jawaban-benar").style.display = "none";
   });
+
+  // Riset Lembar Soal
+  soalKe = 0;
+  jawabanUser = [];
+  seluruhOpsi.forEach((opsi) => {
+    opsi.checked = false;
+    opsi.disabled = false;
+  });
+  selesai = false;
+  document.querySelector(".tombol-selesai").style.display = "block";
+  document.querySelector(".hasil").style.display = "none";
+  document.querySelector(".jawaban-benar").style.display = "none";
+});
 
 // =========================================================
 // Ketika tombol batal-radio diklik
@@ -130,10 +150,10 @@ batalRadio.addEventListener("click", () => {
     batalRadio.style.display = "none";
   }, 50);
 });
-// Ketika tombol selanjutnya atau sebelumnya
+
+// Ketika tombol selanjutnya atau sebelumnya di klik
 const tombol = document.querySelector(".tombol");
 tombol.addEventListener("click", (element) => {
-  // Ketika tombol selanjutnya atau sebelumnya di klik
   if (element.target.classList.contains("tombol-selanjutnya")) soalKe++;
   if (element.target.classList.contains("tombol-sebelumnya")) soalKe--;
   if (soalKe < 0) soalKe = dataHasil.jumlah_soal - 1;
@@ -145,54 +165,52 @@ tombol.addEventListener("click", (element) => {
 });
 
 // Ketika tombol selesai di klik
-document
-  .querySelector(".tombol-selesai")
-  .addEventListener("click", function () {
-    if (confirm("Ntos yakin, Henteu????")) {
-      batalRadio.style.display = "none";
-      soalKe = 0;
-      seluruhOpsi.forEach((opsi) => (opsi.disabled = true));
-      this.style.display = "none";
-      selesai = true;
-      let nilai = 0;
-      seluruhNoSoal.childNodes.forEach((noSoal, index) => {
-        if (
-          jawabanUser[index] == undefined ||
-          jawabanUser[index][0] != dataHasil.soal[index].jawaban_benar
-        ) {
-          noSoal.classList.add("salah");
-        } else {
-          noSoal.classList.add("benar");
-          nilai++;
-        }
-      });
-      nilai = (100 / dataHasil.jumlah_soal) * nilai;
-      const nilaiUser = document.querySelector(".nilai");
-      nilaiUser.innerHTML = nilai;
-      if (nilai > 75) nilaiUser.style.color = "lightgreen";
-      else nilaiUser.style.color = "red";
-      nilaiUser.parentElement.parentElement.style.display = "block";
-      mencetakLembarSoal(dataHasil);
-      gantiNoAktif();
-      aktifKanOpsi();
-    }
-  });
+document.querySelector(".tombol-selesai").addEventListener("click", function () {
+  if (confirm("Ntos yakin, Henteu????")) {
+    batalRadio.style.display = "none";
+    soalKe = 0;
+    seluruhOpsi.forEach((opsi) => (opsi.disabled = true));
+    this.style.display = "none";
+    selesai = true;
+    let nilai = 0;
+    seluruhNoSoal.childNodes.forEach((noSoal, index) => {
+      if (jawabanUser[index] == undefined || jawabanUser[index][0] != dataHasil.soal[index].jawaban_benar) {
+        noSoal.classList.add("salah");
+      } else {
+        noSoal.classList.add("benar");
+        nilai++;
+      }
+    });
+    nilai = (100 / dataHasil.jumlah_soal) * nilai;
+    const nilaiUser = document.querySelector(".nilai");
+    nilaiUser.innerHTML = nilai;
+    if (nilai > 75) nilaiUser.style.color = "lightgreen";
+    else nilaiUser.style.color = "red";
+    nilaiUser.parentElement.parentElement.style.display = "block";
+    mencetakLembarSoal(dataHasil);
+    gantiNoAktif();
+    aktifKanOpsi();
+  }
+});
 
 // Ketika pilihan ganda atau opsi diklik
 seluruhOpsi.forEach((opsi) => {
   opsi.addEventListener("click", function (element) {
-    jawabanUser[soalKe] = [
-      element.target.id,
-      element.target.nextElementSibling.innerHTML,
-    ];
+    jawabanUser[soalKe] = [element.target.id, element.target.nextElementSibling.innerHTML];
     seluruhNoSoal.childNodes[soalKe].classList.add("terpilih");
     batalRadio.style.display = "block";
     console.log(jawabanUser[soalKe][0]);
   });
 });
 
+function modalDitutup() {
+  document.querySelectorAll(".modal").forEach((modal) => {
+    modal.style.display = "none";
+  });
+}
+
 function ambilData() {
-  return fetch(`soal/${document.querySelector("select").value}`)
+  return fetch(`soal/${document.querySelector(".data-accordion-terpilih").dataset.file}`)
     .then((result) => result.json())
     .then((result) => result);
 }
@@ -222,31 +240,18 @@ function mencetakLembarSoal(result) {
   noSoal.innerHTML = result.soal[soalKe].nomor;
   // Mengganti soal
   const soal = document.querySelector(".soal");
-  soal.innerHTML = result.soal[soalKe].pertanyaan
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
+  soal.innerHTML = result.soal[soalKe].pertanyaan.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
   // Mengganti pilihan ganda
   const pilihanGanda = document.querySelectorAll(".pilihan-ganda li label");
-  pilihanGanda[0].innerHTML = result.soal[soalKe].pilihan.a
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
-  pilihanGanda[1].innerHTML = result.soal[soalKe].pilihan.b
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
-  pilihanGanda[2].innerHTML = result.soal[soalKe].pilihan.c
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
-  pilihanGanda[3].innerHTML = result.soal[soalKe].pilihan.d
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;");
+  pilihanGanda[0].innerHTML = result.soal[soalKe].pilihan.a.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+  pilihanGanda[1].innerHTML = result.soal[soalKe].pilihan.b.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+  pilihanGanda[2].innerHTML = result.soal[soalKe].pilihan.c.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+  pilihanGanda[3].innerHTML = result.soal[soalKe].pilihan.d.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
   if (selesai) {
     const jawabanBenar = document.querySelector(".jawaban-benar");
     // Menggati jawaban yang benar
     jawabanBenar.children[1].children[0].children[0].innerHTML = `${result.soal[soalKe].jawaban_benar}`;
-    if (
-      jawabanUser[soalKe] == undefined ||
-      jawabanUser[soalKe][0] != result.soal[soalKe].jawaban_benar
-    ) {
+    if (jawabanUser[soalKe] == undefined || jawabanUser[soalKe][0] != result.soal[soalKe].jawaban_benar) {
       jawabanBenar.style.display = "block";
     } else {
       jawabanBenar.style.display = "none";
